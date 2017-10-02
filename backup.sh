ccdf1cfbbc298d508028552a4148f0bb7be6f442
Marvin Caspar <marvin.caspar@gmail.com>
2017-10-02 09:16:38 +0200

    Use new dropbox api

--- backup.sh
+++ backup.sh
@@ -7,9 +7,8 @@ MYSQL_HOST_OPTS="-h $MYSQL_HOST -P $MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD"
 echo "Creating backup for $MYSQL_DATABASE..."
 mysqldump $MYSQL_HOST_OPTS $MYSQLDUMP_OPTIONS $MYSQL_DATABASE \
   | gzip \
-  | curl -X PUT \
-         -H "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
-         -T - \
-         --silent \
-         --output /dev/null \
-         https://content.dropboxapi.com/2/files/upload/auto/$DROPBOX_PREFIX$MYSQL_DATABASE.sql.gz
+  | curl -X POST https://content.dropboxapi.com/2/files/upload \
+        --header "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
+        --header "Dropbox-API-Arg: {\"path\": \"/$DROPBOX_PREFIX$MYSQL_DATABASE.sql.gz\",\"mode\": \"overwrite\",\"autorename\": false,\"mute\": true}" \
+        --header "Content-Type: application/octet-stream" \
+        --data-binary @$DROPBOX_PREFIX$MYSQL_DATABASE.sql.gz
