1d18f64b6b05957a2223ae4a44c40f209e9815ba
Marvin Caspar <marvin.caspar@gmail.com>
2017-10-02 09:24:49 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -10,5 +10,4 @@ mysqldump $MYSQL_HOST_OPTS $MYSQLDUMP_OPTIONS $MYSQL_DATABASE \
   | curl -X POST https://content.dropboxapi.com/2/files/upload \
         --header "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
         --header "Dropbox-API-Arg: {\"path\": \"/$DROPBOX_PREFIX$MYSQL_DATABASE.sql.gz\",\"mode\": \"overwrite\",\"autorename\": false,\"mute\": true}" \
-        --header "Content-Type: application/octet-stream" \
-        --data-binary @$DROPBOX_PREFIX$MYSQL_DATABASE.sql.gz
+        --header "Content-Type: application/octet-stream"
